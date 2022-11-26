f2c7d0b3b0b2314ee651abd5489caefde5d1a620
ssCuacKss <proldanpuebla@gmail.com>
2022-11-26 16:37:51 +0100

    creado Bean de usuario

---
+++ src/main/java/display/javabean/userBean.java
@@ -0,0 +1,169 @@
+package display.javabean;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import business.usuario.UsuarioDTO;
+
+public class userBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String nombre_;
+	private String apellidos_;
+	private String correo_;
+	private String fechaNacimiento_;
+	private String fechaInscripcion_;
+	private String passWord_;
+	private Boolean admin_;
+	
+	
+	/**
+	 * Constructor vacio (default)
+	 */
+	
+	public userBean() {
+		nombre_ = "";
+		apellidos_ = "";
+		correo_ = "";
+		fechaNacimiento_ = "";
+		fechaInscripcion_ = "";
+		passWord_ = "";
+		admin_ = false;
+	}
+	
+	/**
+	 * Constructor parametrizado
+	 * @param usuario DTO con los datos del usuario obtenidos de la base de datos
+	 */
+	
+	public userBean(UsuarioDTO usuario) {
+		nombre_ = usuario.getNombre();
+		apellidos_ = usuario.getApellidos();
+		correo_ = usuario.getCorreo();
+		fechaNacimiento_ = usuario.getFechaNacimiento();
+		fechaInscripcion_ = usuario.getFechaInscripcion();
+		passWord_ = usuario.getPassWord();
+		admin_ = usuario.getAdmin();
+	}
+	
+	/**
+	 * Getter nombre del usuario
+	 * @return Nombre del usuario
+	 */
+	public String getNombre() {
+		return nombre_;
+	}
+	
+	/**
+	 * Getter apellidos del usuario
+	 * @return Apellidos del usuario
+	 */
+	public String getApellidos() {
+		return apellidos_;
+	}
+	
+	/**
+	 * Getter correo del usuario
+	 * @return Correo del usuario
+	 */
+	public String getCorreo() {
+		return correo_;
+	}
+	
+	/**
+	 * Getter fecha de nacimiento del usuario
+	 * @return Fecha de nacimiento del usuario
+	 */
+	public String getFechaNacimiento() {
+		return fechaNacimiento_;
+	}
+	
+	/**
+	 * Getter fecha de inscripción
+	 * @return Fecha de inscripción
+	 */
+	public String getFechaInscripcion() {
+		return fechaInscripcion_;
+	}
+	
+	/**
+	 * Getter contraseña del usuario
+	 * @return contraseña del usuario
+	 */
+	
+	public String getPassWord(){
+		return this.passWord_;
+	}
+	
+	/**
+	 * Getter Tipo de usuario
+	 * @return Tipo de usuario
+	 */
+	
+	public Boolean getAdmin() {
+		return this.admin_;
+	}
+	
+	/**
+	 * Setter del nombre del usuario
+	 * @param nombre Nombre del usuario
+	 */
+	
+	public void setNombre(String nombre) {
+		this.nombre_ = nombre;
+	}
+	
+	/**
+	 * Setter de los apellidos del usuario
+	 * @param apellidos Apellidos del usuario
+	 */
+	public void setApellidos(String apellidos) {
+		this.apellidos_ = apellidos;
+	}
+
+	/**
+	 * Setter del correo del usuario
+	 * @param correo Correo del usaurio
+	 */
+	public void setCorreo(String correo) {
+		this.correo_ = correo;
+	}
+	
+	/**
+	 * Setter de la fecha de nacimiento del usuario
+	 * @param fechaNacimiento Fecha de nacimiento del usuario
+	 */
+	public void setFechaNacimiento(String fechaNacimiento) {	
+		fechaNacimiento_ = fechaNacimiento;
+	}
+	
+	/**
+	 * Setter de la fecha de inscripcion
+	 * @param fecha_inscripcion Fecha de inscripcion
+	 */
+	public void setFechaInscripcion(String fecha_inscripcion) {
+		fechaInscripcion_ = fecha_inscripcion;
+	}
+	
+	
+	/**
+	 * Permite asignar una contraseña nueva al usuario
+	 * @param passWord Nueva contraseña del usuario
+	 */
+	
+	public void setPassWord(String passWord) {
+		this.passWord_ = passWord;
+	}
+	
+	/**
+	 * Permite asignar el tipo de usuario
+	 * @param admin tipo de usuario a asignar al usuario
+	 */
+	
+	public void setAdmin(Boolean admin) {
+		this.admin_ = admin;
+	}
+	
+}
