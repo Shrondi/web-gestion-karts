f7f8895f991add5e0f61edd41714f5bace5ffd3d
PalomaRODE4 <71469026+PalomaRODE4@users.noreply.github.com>
2022-12-13 01:11:06 +0100

    Servlets alta de karts y pistas

---
+++ src/main/java/servlet/admin/CrearKart.java
@@ -0,0 +1,76 @@
+package servlet.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import data.DAO.KartDAO;
+import display.javabean.userBean;
+
+/**
+ * Servlet implementation class CrearKart
+ */
+@WebServlet("/CrearKart")
+public class CrearKart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher;
+		HttpSession session = request.getSession();
+		ServletContext application = session.getServletContext();
+		userBean userBean = (userBean) session.getAttribute("userBean");
+		
+		//Obtenemos el valor del parametro sqlproperties, es decir, la ruta relativa al fichero sql.properties
+		String sqlProperties= application.getInitParameter("sqlproperties"); 
+		java.io.InputStream myIO = application.getResourceAsStream(sqlProperties);
+		java.util.Properties prop = new java.util.Properties();
+		prop.load(myIO);
+		
+		//Caso 1: Usuario no esta logueado -> Volvemos al index
+		if (userBean == null || userBean.getCorreo().equals(""))
+		{
+			//dispatcher = request.getRequestDispatcher("/index.jsp");
+			//dispatcher.forward(request, response);
+			response.sendRedirect("/WebProyectoPW");
+		}
+		
+		//Caso 2: Usuario logueado y creamos kart
+		else
+		{
+			String tipo = (String) request.getSession().getAttribute("tipo");
+			String estado = (String) request.getSession().getAttribute("estado");
+			
+			KartDAO kartDAO = new KartDAO(prop);
+			
+			//REVISAR SI HACE FALTA ESTOS CONTROLES O NO
+			boolean estado_control = false;
+			boolean tipo_control = false;
+			
+			//Control de estado
+			if(estado.toUpperCase().contains("DISPONIBLE") || estado.toUpperCase().contains("RESERVADO") || estado.toUpperCase().contains("MANTENIMIENTO")) {
+				estado_control = true;
+			}
+			//Control de tipo
+			if(tipo.toUpperCase().contains("INFANTIL") || tipo.toUpperCase().contains("ADULTOS")) {
+				tipo_control = true;
+			}
+		}	
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+}
---
+++ src/main/java/servlet/admin/CrearPista.java
@@ -0,0 +1,83 @@
+package servlet.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import data.DAO.PistaDAO;
+import display.javabean.userBean;
+
+/**
+ * Servlet implementation class CrearPista
+ */
+@WebServlet("/CrearPista")
+public class CrearPista extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher;
+		HttpSession session = request.getSession();
+		ServletContext application = session.getServletContext();
+		userBean userBean = (userBean) session.getAttribute("userBean");
+		
+		//Obtenemos el valor del parametro sqlproperties, es decir, la ruta relativa al fichero sql.properties
+		String sqlProperties= application.getInitParameter("sqlproperties"); 
+		java.io.InputStream myIO = application.getResourceAsStream(sqlProperties);
+		java.util.Properties prop = new java.util.Properties();
+		prop.load(myIO);
+		
+		//Caso 1: Usuario no esta logueado -> Volvemos al index
+		if (userBean == null || userBean.getCorreo().equals(""))
+		{
+			//dispatcher = request.getRequestDispatcher("/index.jsp");
+			//dispatcher.forward(request, response);
+			response.sendRedirect("/WebProyectoPW");
+		}
+		
+		//Caso 2: Usuario logueado, creamos la pista
+		else
+		{
+			String nombre_pista = (String) request.getSession().getAttribute("nombre");
+			String estado = (String) request.getSession().getAttribute("estado");
+			String dificultad = (String) request.getSession().getAttribute("dificultad");
+			int max_karts = (int) request.getSession().getAttribute("max_karts");
+			
+			PistaDAO pistaDAO = new PistaDAO(prop);
+			
+			//REVISAR SI HACE FALTA ESTOS CONTROLES O NO
+			boolean estado_control = false;
+			boolean dificultad_control = false;
+			boolean numero_karts = true;
+			
+			//Control de estado
+			if(estado.toUpperCase().contains("DISPONIBLE") || estado.toUpperCase().contains("RESERVADO") || estado.toUpperCase().contains("MANTENIMIENTO")) {
+				estado_control = true;
+			}
+			//Control de dificultad
+			if(dificultad.toUpperCase().contains("INFANTIL") || dificultad.toUpperCase().contains("FAMILIAR") || dificultad.toUpperCase().contains("ADULTOS")) {
+				dificultad_control = true;
+			}
+			//Control de numero maximo de karts
+			if(max_karts <= 0) {
+				numero_karts = false;
+			}
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+}
