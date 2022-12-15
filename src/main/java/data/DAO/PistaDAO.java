b076f303b03278a111e7d047bd9b31b03e361b91
PalomaRODE4 <71469026+PalomaRODE4@users.noreply.github.com>
2022-12-15 18:19:30 +0100

    DAO Pista Actualizado

--- src/main/java/data/DAO/PistaDAO.java
+++ src/main/java/data/DAO/PistaDAO.java
@@ -1,8 +1,6 @@
 package data.DAO;
 
 import data.common.DBConnection;
-import business.kart.Estado;
-import business.kart.KartDTO;
 import business.pista.Dificultad;
 import business.pista.PistaDTO;
 
@@ -91,13 +89,15 @@ public class PistaDAO{
 			while (rs.next()) {
 				String nombre = rs.getString("nombre");
 				String dificultad = rs.getString("dificultad");
-				int max_karts = rs.getInt("max_karts");
-				int asoc_karts = rs.getInt("asoc_karts");
+				int max_karts = rs.getInt("max_karts");			
+				int asoc_karts_infantiles = rs.getInt("asoc_karts_disp_infantiles");
+				int asoc_karts_adultos = rs.getInt("asoc_karts_disp_adultos");
 				PistaDTO pistaToPush = new PistaDTO();
 				pistaToPush.setNombre(nombre);
 				pistaToPush.setDificulty(Dificultad.valueOf(dificultad.toUpperCase()));
 				pistaToPush.setMaxAmmount(max_karts);
-				//pistaToPush.setAsocAmmount(asoc_karts);
+				pistaToPush.setAsocAmmountInf(asoc_karts_infantiles);
+				pistaToPush.setAsocAmmountAdult(asoc_karts_adultos);
 				pistaToPush.setEstado(estado);
 				pistas.add(pistaToPush);
 			}
@@ -140,6 +140,106 @@ public class PistaDAO{
 	}
 	
 	//NEW
+	/**
+	 * Obtener pistas por su dificultad (infantil,familiar,adultos)
+	 * @param dificultad Tipo de pista
+	 * @return pistas Listado de pistas
+	 */
+	
+	public List<PistaDTO> consultarByDif(String dificultad){
+		List<PistaDTO> pistas = new ArrayList<>();
+		DBConnection connection = new DBConnection();
+		con = connection.getConnection();
+		try {
+			Statement stmt = con.createStatement();
+			String query = prop.getProperty("obtenerPistasbyDifSTM")+dificultad;
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String nombre = rs.getString("nombre");
+				Boolean estado = rs.getBoolean("estado");
+				int max_karts = rs.getInt("max_karts");
+				int asoc_karts_infantiles = rs.getInt("asoc_karts_disp_infantiles");
+				int asoc_karts_adultos = rs.getInt("asoc_karts_disp_adultos");
+				
+				PistaDTO pistaToPush = new PistaDTO();
+				pistaToPush.setNombre(nombre);
+				pistaToPush.setEstado(estado);
+				pistaToPush.setMaxAmmount(max_karts);
+				pistaToPush.setAsocAmmountInf(asoc_karts_infantiles);
+				pistaToPush.setAsocAmmountAdult(asoc_karts_adultos);
+				pistaToPush.setDificulty(Dificultad.valueOf(dificultad.toUpperCase()));
+				pistas.add(pistaToPush);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch(IllegalArgumentException e){
+			e.printStackTrace();
+		}
+		
+		connection.closeConnection();
+		return pistas;
+	}
+	
+	//NEW
+	/**
+	 * Obtener pistas por su numero de karts infantiles y karts adultos
+	 * @param kartsInfantiles Numero de karts infantiles
+	 * @param kartsAdultos Numero de karts adultos
+	 * @return pistas Listado de pistas
+	 */
+	public List<PistaDTO> consultarByMinKarts(int kartsInfantiles, int kartsAdultos){
+		List<PistaDTO> pistas = new ArrayList<>();
+		DBConnection connection = new DBConnection();
+		con = connection.getConnection();
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(prop.getProperty("obtenerPistasByMinKartsSTM"));
+			
+			ps.setInt(1, kartsInfantiles);
+			ps.setInt(2, kartsAdultos);
+		
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+
+				String nombre = rs.getString("nombre");
+				Boolean estado = rs.getBoolean("estado");
+				String dificultad = rs.getString("dificultad");
+				int max_karts = rs.getInt("max_karts");
+				int asoc_karts_infantiles = rs.getInt("asoc_karts_disp_infantiles");
+				int asoc_karts_adultos = rs.getInt("asoc_karts_disp_adultos");
+				
+				PistaDTO pistaToPush = new PistaDTO();
+				pistaToPush.setNombre(nombre);
+				pistaToPush.setEstado(estado);
+				pistaToPush.setDificulty(Dificultad.valueOf(dificultad.toUpperCase()));
+				pistaToPush.setMaxAmmount(max_karts);
+				pistaToPush.setAsocAmmountInf(asoc_karts_infantiles);
+				pistaToPush.setAsocAmmountAdult(asoc_karts_adultos);
+				
+				pistas.add(pistaToPush);
+			}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} catch(IllegalArgumentException e){
+			e.printStackTrace();
+		}
+		
+		connection.closeConnection();
+		return pistas;
+	}	
+	
+	//NEW
+	/**
+	 * Obtener pistas por su tipo y numero de karts infantiles y karts adultos
+	 * @param tipoPista Tipo de pista (infantil,familiar,adultos)
+	 * @param kartsInfantiles Numero de karts infantiles
+	 * @param kartsAdultos Numero de karts adultos
+	 * @return pistas Listado de pistas
+	 */
 	public List<PistaDTO> consultarPistas(String tipoPista, int kartsInfantiles, int kartsAdultos){
 		List<PistaDTO> pistas = new ArrayList<>();
 		DBConnection connection = new DBConnection();
